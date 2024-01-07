e291e11e8dacaee480108b308771ba5c02c1501a
Thiago Leão <thiagogorgulho@outlook.com>
2024-01-07 13:41:44 -0300

    Add HTTP method and path validation
    This commit adds conditional checks to ensure that the correct HTTP method is used for `snippetCreate` and the correct URL path for `home`. If the checks fail, the server now sends the appropriate HTTP status codes and messages.

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippetbox"))
 }
 
@@ -14,6 +18,11 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Create a new snippet"))
 }
 
