1a6c84094e029ee6e69d9dbea58f2659fa04f4d7
Thiago Leão <thiagogorgulho@outlook.com>
2024-02-02 19:56:48 +0000

    Refactor template handling and update HTML templates
    Reconfigured the template handling in the view feature to use a templateData struct instead of individual properties for improved efficiency. Updated the HTML view template and handlers accordingly to align with the changes implemented.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,35 +15,32 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//files := []string{
-	//	"./ui/html/base.tmpl",
-	//	"./ui/html/partials/nav.tmpl",
-	//	"./ui/html/pages/home.tmpl",
-	//}
-
-	//ts, err := template.ParseFiles(files...)
-
-	//if err != nil {
-	//	app.serverError(w, r, err)
-	//	return
-	//}
-
 	snippets, err := app.snippetsModel.Latest(5)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
 	}
 
-	//err = ts.ExecuteTemplate(w, "base", nil)
-	//
-	//if err != nil {
-	//	app.serverError(w, r, err)
-	//	return
-	//}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	data := templateData{Snippets: snippets}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 	app.logger.Info("served home")
 }
 
